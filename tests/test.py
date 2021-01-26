ef8a20777412de56215f4d7a6333d60b5acbf10f
Elias Gabriel <me@eliasfgabriel.com>
2021-01-25 23:06:57 -0500

    specify local .venv, simplify vscode settings

--- tests/test.py
+++
@@ -1,7 +0,0 @@
-# import doveflow
-# from doveflow import A, B, C, D, E
-
-# def async_test():
-#     canvas = Workflow(Group(Collaborative(A, B, C), onerror=, D, onerror=), E, onerror=)
-
-#        result = canvas.run()
